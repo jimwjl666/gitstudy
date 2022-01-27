e01a72809e50c434a3eaf59e0dd258c21797fd0e
gitwujiaolong <742265483@qq.com>
2022-01-27 19:18:50 +0800

    feat:增加functionC

--- testA.js
+++ testA.js
@@ -5,10 +5,8 @@ function main() {
   console.log('main')
 }
 function a2() {
-  console.log('a2')
+  console.log('我来自分支branchA')
 }
 function C() {
   console.log('branchC')
-}function C() {
-  console.log('DEV')
 }
